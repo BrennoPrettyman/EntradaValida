d210527e15b3bf52d1998666677b63aaefafe6df
brennoprettyman <brennoprettyman@gmail.com>
2023-06-11 12:29:09 -0300

    Exercício Concluido

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+int valor = -1;
+
+Console.WriteLine("--- Entrada Válida ---");
+
+while(valor < 0 || valor > 9 )
+{
+    Console.Write("Digite um valor entre 1 e 9 (0 para cancelar): ");
+    valor = Convert.ToInt32(Console.ReadLine());
+}
+
+if (valor == 0)
+{
+    Console.WriteLine($"Operação cancelada.");
+}
+
+else
+{
+    Console.WriteLine($"\nO número escolhido foi {valor}.");
+}
+
+
